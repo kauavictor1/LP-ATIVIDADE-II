7888a9737e434dcc4516d2d9143d146618c23cf6
kauavictor1 <144855462+kauavictor1@users.noreply.github.com>
2023-11-08 14:18:32 -0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	int i;
+	
+	
+	printf("escreva um numero: ");
+	scanf("%d",&i);
+	
+	while(i >= 0){
+	printf("%d \n",i);
+
+i=i-1;	
+}
+	
+printf("cabo");	
+	return 0;
+}
